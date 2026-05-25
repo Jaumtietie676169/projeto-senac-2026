ca3b9aa376a31cac78f43a822427cf3ceaa273c2
JOAO VICTOR PENNA DA SILVA <60240668057@senacrs.edu.br>
2026-05-25 17:13:47 -0300

    Animação de trocar troca de tela

---
+++ app/scripts/menu.js
@@ -0,0 +1,5 @@
+const toggleButton = document.querySelector('#Container header .toggle-button');
+
+const headerElement = document.querySelector('#Container header');
+
+toggleButton.addEventListener(click)
