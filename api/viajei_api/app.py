3f719e5ee27e4c81662508a8db3f8c6522c934ac
JOAO VICTOR PENNA DA SILVA <60240668057@senacrs.edu.br>
2026-06-24 17:26:22 -0300

    API 2

--- api/viajei_api/app.py
+++ api/viajei_api/app.py
@@ -1,8 +1,38 @@
+from http import HTTPStatus
+
+from api.viajei_api.schemas.user import User, UserDB, UserPublic
 from fastapi import FastAPI
 
 app = FastAPI()
 
+database = []
+
 
 @app.get("/")
+def read_root():
+    return {"message": "Bem vindo!"}
+
+
+@app.get("/")
+def bem_vindo():
+    return {"message": "Bem vindo!"}
+
+
+@app.get("/hello")
 def ola_mundo():
-    return {"Olá! Turma"}
+    return """
+        <head>
+        </head>
+        <body>
+            <h1> </h1>
+        </body>
+"""
+
+
+@app.post("/auth/", status_code=HTTPStatus.CREATED, response_model=UserPublic)
+def create_user(user: User):
+    user_with_id = UserDB(**user.model(), id=len(database) + 1)
+
+    database.append(user_with_id)
+
+    return user_with_id
